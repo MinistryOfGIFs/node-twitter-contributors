bb78d0f566c812673aae9cfa412ce51435f342db
Jordan Harband <ljharb@gmail.com>
2013-07-02 02:10:49 -0700

    Using dot notation instead of bracket notation.

--- daemon.js
+++ daemon.js
@@ -30,8 +30,8 @@ var Twit = new twitter ({
 function Tweet (status) {
   Twit.updateStatus(status,
     function(data) {
-      if (data["id_str"]){
-        console.log("Tweet sent: " + data["text"]);
+      if (data.id_str) {
+        console.log("Tweet sent: " + data.text);
       }
     }
   );
@@ -41,8 +41,8 @@ function DM (user_id, text) {
   console.log("DM: " + user_id);
   Twit.newDirectMessage(user_id, text,
     function(data) {
-      if (data["recipient"]){
-        console.log("DM sent: " + data["text"])
+      if (data.recipient) {
+        console.log("DM sent: " + data.text)
       }
     }
   );
@@ -65,17 +65,17 @@ function handle_event (event, data){
   switch (event)
   {
     case "follow":
-      console.log("Follow event: " + data["source"]["id_str"] + " => " + data["target"]["id_str"]);
-      if (data["source"]["id_str"] === config.user_id){
-        console.log("Added @" + data["target"]["screen_name"] + " to friends.")
-        friends.push(data["target"]["id_str"]);
+      console.log("Follow event: " + data.source.id_str + " => " + data.target.id_str);
+      if (data.source.id_str === config.user_id) {
+        console.log("Added @" + data.target.screen_name + " to friends.")
+        friends.push(data.target.id_str);
       }
     break;
     case "unfollow":
-    console.log("Unfollow event: " + data["source"]["id_str"] + " => " + data["target"]["id_str"]);
-      if (data["source"]["id_str"] === config.user_id){
-        console.log("Removed @" + data["target"]["screen_name"] + " from friends.")
-        friends = _.without(friends, data["target"]["id_str"]);
+    console.log("Unfollow event: " + data.source.id_str + " => " + data.target.id_str);
+      if (data.source.id_str === config.user_id) {
+        console.log("Removed @" + data.target.screen_name + " from friends.")
+        friends = _.without(friends, data.target.id_str);
       }
     break;
   }
@@ -94,11 +94,11 @@ function handle_queue (id) {
     function tweet_links() {
       console.log("exec tweet_links");
       clearTimeout(tweetTimer);
-      if (tweetQueue[message_id]["urls"].length) {
-        var tweetURL = tweetQueue[message_id]["urls"].pop();
+      if (tweetQueue[message_id].urls.length) {
+        var tweetURL = tweetQueue[message_id].urls.pop();
         console.log(tweetURL);
         Tweet(tweetURL);
-        tweetQueue[message_id]["urls"].length;
+        tweetQueue[message_id].urls.length;
         messageTimer = setTimeout(function(){tweet_links()}, 15000);
       }else{
         delete tweetQueue[message_id];
@@ -114,9 +114,9 @@ function handle_queue (id) {
 }
 
 function parse_dm_blob (data){
-  var message_id = data["direct_message"]["id_str"],
-      sender_id  = data["direct_message"]["sender"]["id_str"],
-      screen_name  = data["direct_message"]["sender"]["screen_name"];
+  var message_id = data.direct_message.id_str,
+      sender_id  = data.direct_message.sender.id_str,
+      screen_name  = data.direct_message.sender.screen_name;
 
   console.log("DM from @" + screen_name + " " + sender_id);
 
@@ -124,23 +124,23 @@ function parse_dm_blob (data){
 
     var tmpQueue = {};
 
-    tmpQueue["message_id"] = message_id;
-    tmpQueue["sender_id"]  = sender_id;
-    tmpQueue["sender"]     = data["direct_message"]["sender"]["screen_name"];
-    tmpQueue["created_at"] = data["direct_message"]["created_at"];
+    tmpQueue.message_id = message_id;
+    tmpQueue.sender_id = sender_id;
+    tmpQueue.sender = data.direct_message.sender.screen_name;
+    tmpQueue.created_at = data.direct_message.created_at;
     tmpQueue.urls = findUrls(data.direct_message.text);
 
     tweetQueue[message_id] = tmpQueue;
 
     if (tweetQueue[message_id]){
-      if (tweetQueue[message_id]["urls"].length > 1){
-        DM(parseInt(sender_id), "Received " + tweetQueue[message_id]["urls"].length + " links");
-        _.each(tweetQueue[message_id]["urls"], function(url, index){
+      if (tweetQueue[message_id].urls.length > 1){
+        DM(parseInt(sender_id), "Received " + tweetQueue[message_id].urls.length + " links");
+        _.each(tweetQueue[message_id].urls, function(url, index){
           Tweet(url);
         });
       }else{
-        DM(parseInt(sender_id), "Received " + tweetQueue[message_id]["urls"].length + " link");
-        Tweet(tweetQueue[message_id]["urls"][0]);
+        DM(parseInt(sender_id), "Received " + tweetQueue[message_id].urls.length + " link");
+        Tweet(tweetQueue[message_id].urls[0]);
       }
 
     }
@@ -168,18 +168,18 @@ function stringIt(v){
 }
 
 userStream.on("data", function(data) {
-  if (data["warning"]){
+  if (data.warning) {
     console.log("WARNING");
-    DM(parseInt(config.admin_id), " WARNING: [" + data["code"] + "] " + data["message"]);
+    DM(parseInt(config.admin_id), " WARNING: [" + data.code + "] " + data.message);
   }
-  if (data["friends"]){
-    friends = data["friends"].map(String); // TODO: Update this for 64bit user IDs
+  if (data.friends) {
+    friends = data.friends.map(String); // TODO: Update this for 64bit user IDs
     console.log("Loaded friends");
   }
-  if (data["event"]){
-    handle_event(data["event"], data);
+  if (data.event) {
+    handle_event(data.event, data);
   }  
-  if (data["direct_message"]){
+  if (data.direct_message) {
     parse_dm_blob(data);
   };
   // console.log(data);
