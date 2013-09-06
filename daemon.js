61513f6362d4887cdc28cdfd3c80ea9db83184f4
Roach <roach@Roach.local>
2013-09-06 02:25:01 -0700

    Improved error handling, cleanup.

--- daemon.js
+++ daemon.js
@@ -1,16 +1,16 @@
 var util = require("util"),
-  twitter = require("twitter"),
-  Stream = require("user-stream"),
-  fs = require('fs'),
-  config = require("./config.json"), // See config-sample.json
-  friends = [], // Users this account follows
-  tweetQueue = {},
-  environment = "dev", // 'dev' for development or 'prod' for production
-  show_heartbeat = true, // logs '--^v--' to stdout only
-  heartbeat_timer = null,
-  logfile = "log.txt"; // name of the file you want log messages to output to
+    twitter = require("twitter"),
+    Stream = require("user-stream"),
+    fs = require('fs');
+    environment = "dev", // 'dev' for development or 'prod' for production
+    config_file = require("./config.json"), // See config-sample.json
+    config = config_file[environment],
+    logfile = config.screen_name + "log.txt", // name of the file you want log messages to output to
+    friends = [], // Users this account follows
+    tweet_queue = {},
+    show_heartbeat = true, // logs '--^v--' to stdout only
+    heartbeat_timer = null;
 
-config = config[environment];
 // Misc helpers
 
 function padNum (n) {
@@ -46,20 +46,27 @@ function parseURLs (text) {
 }
 
 function heartbeatTimer (timeout) {
-  timeout = timeout
+  timeout = timeout || 0
   heartbeat_timer = setInterval(function(){
     if (timeout > 1){
       timeout--
     }else{
-      log(timestamp() + " Heartbeat timed out, reconnectiong...");
+      log(timestamp() + " Heartbeat timed out, reconnecting...");
       clearInterval(heartbeat_timer);
-      userStream.destroy();
-      initStream();
+      reconnectStream()
     }
   },1000);
 }
 
-// twttr functions
+// twttr setup and helpers
+
+var twttr = new twitter ({
+  consumer_key: config.consumer_key,
+  consumer_secret: config.consumer_secret,
+  access_token_key: config.oauth_token,
+  access_token_secret: config.oauth_secret,
+  rest_base: "https://api.twitter.com/1.1"
+});
 
 function sendTweet (status) {
   twttr.updateStatus(status,
@@ -72,7 +79,7 @@ function sendTweet (status) {
 }
 
 function sendDM (user_id, text) {
-  twttr.newDirectMessage(user_id, text,
+  twttr.newDirectMessage(parseInt(user_id), text,
     function (data) {
       if (data.recipient) {
         log(timestamp() + " DM sent to @" + data.recipient.screen_name + ": " + data.text)
@@ -81,18 +88,55 @@ function sendDM (user_id, text) {
   );
 }
 
-// userStream helpers
+// userStream setup and helpers
+
+var userStream = new Stream({
+  consumer_key: config.consumer_key,
+  consumer_secret: config.consumer_secret,
+  access_token_key: config.oauth_token,
+  access_token_secret: config.oauth_secret,
+});
+
+function initStream(){
+  // Verify credentials and connect if successful
+  twttr.verifyCredentials(function (data) {
+    if (data.id_str){
+      userStream.stream();
+      heartbeatTimer(120);
+    } else {
+      log(timestamp() + " Error\n" + util.inspect(data, {depth:null}));
+      log(timestamp() + " Connection failed, retrying in 2 minutes...");
+      reconnectStream(120);
+    }
+  })
+}
+
+function reconnectStream(timeout){
+  // Kill current connection and reconnect
+  timeout = timeout || 0
+  reconnect_timer = setTimeout(function(){
+    userStream.destroy();
+    initStream();
+  }, (timeout * 1000));
+}
 
 function handleEvent (event, data){
   switch (event)
   {
     case "follow":
+      // Handle follow events for the authed user as well as incoming follows
       if (data.source.id_str === config.user_id) {
         friends.push(data.target.id_str);
         log(timestamp() + " Added @" + data.target.screen_name + " to friends.")
+      }else{
+        // Notify the admin when followed by a user with more than x followers
+        if (parseInt(data.source.followers_count) > 2000){
+          sendDM(config.admin_id, timestamp() + " Followed by @" + data.source.screen_name + " (" + data.source.followers_count + " followers)");
+        };
       }
     break;
     case "unfollow":
+      // This event is only available for the current authed user. This is not received when a user unfollows you.
       if (data.source.id_str === config.user_id) {
         friends = friends.filter(function (friend) {
           return friend !== data.target.id_str;
@@ -104,6 +148,7 @@ function handleEvent (event, data){
 }
 
 function parseDM (data){
+  // Handle incoming DMs
   var message_id = data.direct_message.id_str,
     sender_id  = data.direct_message.sender.id_str,
     screen_name  = data.direct_message.sender.screen_name;
@@ -114,7 +159,7 @@ function parseDM (data){
 
     if (sender_id === config.admin_id) {
       if (data.direct_message.text === "ping"){
-        sendDM(parseInt(sender_id), timestamp() + " pong!")        
+        sendDM(sender_id, timestamp() + " pong!")
       }
     }
 
@@ -128,17 +173,17 @@ function parseDM (data){
         urls: parseURLs(data.direct_message.text)
       };
 
-      tweetQueue[message_id] = tmpQueue;
+      tweet_queue[message_id] = tmpQueue;
 
-      if (tweetQueue[message_id] && tweetQueue[message_id].urls.length > 0){
-        if (tweetQueue[message_id].urls.length > 1){
-          sendDM(parseInt(sender_id), timestamp() + " Received " + tweetQueue[message_id].urls.length + " links: \n" + tweetQueue[message_id].urls.join(" \n"));
-          tweetQueue[message_id].urls.forEach(function (url) {
+      if (tweet_queue[message_id] && tweet_queue[message_id].urls.length > 0){
+        if (tweet_queue[message_id].urls.length > 1){
+          sendDM(sender_id, timestamp() + " Received " + tweet_queue[message_id].urls.length + " links: \n" + tweet_queue[message_id].urls.join(" \n"));
+          tweet_queue[message_id].urls.forEach(function (url) {
             sendTweet(url);
           });
         }else{
-          sendDM(parseInt(sender_id), timestamp() + " Received " + tweetQueue[message_id].urls.length + " link: \n" + tweetQueue[message_id].urls[0]);
-          sendTweet(tweetQueue[message_id].urls[0]);
+          sendDM(sender_id, timestamp() + " Received " + tweet_queue[message_id].urls.length + " link: \n" + tweet_queue[message_id].urls[0]);
+          sendTweet(tweet_queue[message_id].urls[0]);
         }
 
       }
@@ -148,52 +193,21 @@ function parseDM (data){
   }
 
 }
-
-// Create Twitter REST API and user-stream clients
-
-var twttr = new twitter ({
-  consumer_key: config.consumer_key,
-  consumer_secret: config.consumer_secret,
-  access_token_key: config.oauth_token,
-  access_token_secret: config.oauth_secret,
-  rest_base: "https://api.twitter.com/1.1"
-});
-
-var userStream = new Stream({
-  consumer_key: config.consumer_key,
-  consumer_secret: config.consumer_secret,
-  access_token_key: config.oauth_token,
-  access_token_secret: config.oauth_secret,
-});
-
-// Verify credentials and connect if successful
-function initStream(){
-  twttr.verifyCredentials(function (data) {
-    if (data.id_str){
-      userStream.stream();
-      heartbeatTimer(120);
-    } else {
-      log(timestamp() + " Error");
-      log(util.inspect(data, {depth:null}));
-      log(timestamp() + " Connection failed, retrying in 120 seconds...");
-      heartbeatTimer(120);
-    }
-  })
-}
+// Initialize userStream
 
 initStream();
 
 // userStream listeners
 
 userStream.on("connected", function (data) {
-  log(timestamp() + " Connected to " + config.screen_name);
-  sendDM(parseInt(config.admin_id), timestamp() + " Connected to " + config.screen_name);
+  log(timestamp() + " Connected.");
+  sendDM(config.admin_id, timestamp() + " Connected.");
 });
 
 userStream.on("data", function (data) {
   if (data.warning) {
     log(timestamp() + " WARNING");
-    sendDM(parseInt(config.admin_id), timestamp() + " WARNING: [" + data.code + "] " + data.message);
+    sendDM(config.admin_id, timestamp() + " WARNING: [" + data.code + "] " + data.message);
   }
   if (data.friends) {
     friends = data.friends.map(String); // TODO: Update this for 64bit user IDs
@@ -207,17 +221,36 @@ userStream.on("data", function (data) {
 });
 
 userStream.on("error", function (error) {
-  log(timestamp() + " Error:");
-  log(error);
-  log(util.inspect(error, {depth:null}));
-  sendDM(parseInt(config.admin_id), timestamp() + " ERROR" + error);
+  log(timestamp() + " Error:\n" + util.inspect(error, {depth:null}));
+
+  if (error[type] = 'response'){
+    var errorCode = error[data][code];
+    switch (errorCode){
+      case "420":
+        sendDM(config.admin_id, timestamp() + " ERROR 420: Rate limited, Reconnecting in 10 minutes.");
+        reconnectStream(600);
+        break;
+      case "503":
+        sendDM(config.admin_id, timestamp() + " ERROR 503: Reconnecting in 2 minutes.");
+        reconnectStream(240);
+        break;
+      default:
+        sendDM(config.admin_id, timestamp() + " ERROR: " + errorCode);
+    };
+  };
+
+  if (error[type] = 'request'){
+      sendDM(config.admin_id, timestamp() + " SOCKET ERROR: Reconnecting in 2 minutes.");
+      reconnectStream(240);
+    };
+
 });
 
 userStream.on("close", function (error) {
   log(timestamp() + " Closed:");
   log(error);
   log(timestamp() + " Reconnecting...")
-  sendDM(parseInt(config.admin_id), timestamp() + " Reconnecting...");
+  sendDM(config.admin_id, timestamp() + " Reconnecting...");
   userStream.destroy();
   userStream.stream();
 });
